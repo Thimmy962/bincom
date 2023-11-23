03558b9466e825bd88a627b69008830b6ae9192f
Dada Oluwatimileyin <107270439+Thimmy962@users.noreply.github.com>
2023-11-23 05:08:43 +0100

    Update settings.py

--- bincom_db_test/bincom_db_test/settings.py
+++ bincom_db_test/bincom_db_test/settings.py
@@ -37,8 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'bincom',
-    'rest_framework'
+    'bincom'
 ]
 
 MIDDLEWARE = [
